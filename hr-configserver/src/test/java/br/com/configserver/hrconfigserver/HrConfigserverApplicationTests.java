8ea36c649e91bd2160d172ff14c8829de4f4f9f4
RickFerry <rick_ferry1@hotmail.com>
2023-06-11 14:14:23 -0300

    Project config server created

---
+++ hr-configserver/src/test/java/br/com/configserver/hrconfigserver/HrConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.configserver.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
