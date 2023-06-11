87d6123353455facb6f58231f46fad4b9da8e00b
RickFerry <rick_ferry1@hotmail.com>
2023-06-11 13:55:09 -0300

    Configuring gateway

---
+++ hr-gatewayzuul/src/test/java/br/com/gatewayzuul/hrgatewayzuul/HrGatewayzuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gatewayzuul.hrgatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGatewayzuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
